7c6253843cf1bd47a2e48d627d64beea1935b44d
zpk2017 <zgdisgod@126.com>
2018-05-23 00:00:59 +0800

    [Add: smtp mail service]

--- blog/errors.py
+++ blog/errors.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import render_template
 
 from blog import app, db
@@ -11,4 +14,21 @@ def not_found_page(error):
 @app.errorhandler(500)
 def interval_error(error):
     db.session.rollback()
+    if not app.debug:
+        if app.config["MAIL_SERVER"]:
+            auth = None
+            if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+                auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+            secure = None
+            if app.config["MAIL_USE_TLS"]:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"]),
+                fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+                toaddrs=app.config["ADMINS"],
+                subject="Myblog Failure",
+                credentials=auth,
+                secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
     return render_template("500.html"), 500
