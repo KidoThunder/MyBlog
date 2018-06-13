b77c4055e1be6e6662d05b28a2a9d5247339d28f
zpk2017 <zgdisgod@126.com>
2018-06-13 22:47:31 +0800

    [Update: use rotatingfilehandler to log]

--- blog/errors.py
+++ blog/errors.py
@@ -1,5 +1,7 @@
 import logging
-from logging.handlers import SMTPHandler
+# from logging.handlers import SMTPHandler
+import os
+from logging.handlers import RotatingFileHandler
 
 from flask import render_template
 
@@ -15,20 +17,32 @@ def not_found_page(error):
 def interval_error(error):
     db.session.rollback()
     if not app.debug:
-        if app.config["MAIL_SERVER"]:
-            auth = None
-            if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
-                auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
-            secure = None
-            if app.config["MAIL_USE_TLS"]:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"]),
-                fromaddr="no-reply@" + app.config["MAIL_SERVER"],
-                toaddrs=app.config["ADMINS"],
-                subject="Myblog Failure",
-                credentials=auth,
-                secure=secure)
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
+        # if app.config["MAIL_SERVER"]:
+        #     auth = None
+        #     if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+        #         auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        #     secure = None
+        #     if app.config["MAIL_USE_TLS"]:
+        #         secure = ()
+        #     mail_handler = SMTPHandler(
+        #         mailhost=(app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"]),
+        #         fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+        #         toaddrs=app.config["ADMINS"],
+        #         subject="Myblog Failure",
+        #         credentials=auth,
+        #         secure=secure)
+        #     mail_handler.setLevel(logging.ERROR)
+        #     app.logger.addHandler(mail_handler)
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+
+        file_handler = RotatingFileHandler("logs/myblog.log", maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info("MyBlog startup")
     return render_template("500.html"), 500
