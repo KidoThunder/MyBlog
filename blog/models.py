3342c33661ee3a60f8c738df17d23b5daf0933fb
zpk2017 <zgdisgod@126.com>
2018-06-13 23:41:06 +0800

    [Add: followers]

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from blog import db, login
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
 
 @login.user_loader
 def load_user(uid):
@@ -21,6 +26,13 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+    followed = db.relationship(
+        "User", secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"), lazy="dynamic"
+    )
+
     def __repr(self):
         return "<User {}>".format(self.username)
 
@@ -33,6 +45,19 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.fillter(
+            followers.c.followed_id == user.id
+        ).count() > 0
 
 
 class Post(db.Model):
