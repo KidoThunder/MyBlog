c5e7f7091efbdd5c0117e92d9328d42bf976ac18
guodong.zhao <guodong.zhao@seek4paws.com>
2018-10-29 15:16:58 +0800

    [Update: reformat]

--- blog/errors.py
+++ blog/errors.py
@@ -36,10 +36,13 @@ def interval_error(error):
         if not os.path.exists("logs"):
             os.mkdir("logs")
 
-        file_handler = RotatingFileHandler("logs/myblog.log", maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-        ))
+        file_handler = RotatingFileHandler(
+            "logs/myblog.log", maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(
+            logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'
+            ))
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
 
